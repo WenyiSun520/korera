08ae068e1c1c5e9a6db207eae3bf30ba9f78f798
Wenyi Sun <winnieee.sun@gmail.com>
2023-09-26 12:40:08 -0400

    initilize springboot

---
+++ src/main/java/com/itlize/korera/KoreraApplication.java
@@ -0,0 +1,13 @@
+package com.itlize.korera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoreraApplication.class, args);
+    }
+
+}
